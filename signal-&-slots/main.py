730a579cc7fa941c9ba86782bd58ed26d792cb89
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-01 12:19:33 +0300

    working with check boxes

--- signal-&-slots/main.py
+++ signal-&-slots/main.py
@@ -1,13 +1,14 @@
 import sys
 from PySide6.QtWidgets import QApplication, QPushButton
 
-def button_clicked():
-    print("you clicked this button, didn't you?")
+def button_clicked(data):
+    print("you clicked this button, button checked:", data)
 
 app = QApplication()
 
 button = QPushButton()
 button.setText('Click me!')
+button.setCheckable(True)
 
 button.clicked.connect(button_clicked)
 
