a1e57e69fa44a320e17c12faa4d49ffa7ae1479c
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-04 22:18:26 +0300

    working with signals for cursorPositionChanged, editingFinished, selectionChanged, textEdited

--- Qlabel-QLineEdit/widget.py
+++ Qlabel-QLineEdit/widget.py
@@ -9,7 +9,11 @@ class Widget(QWidget):
         label = QLabel('Fullname: ')
         self.line_edit = QLineEdit()
 
-        self.line_edit.textChanged.connect(self.text_changed)
+        # self.line_edit.textChanged.connect(self.text_changed)
+        # self.line_edit.cursorPositionChanged.connect(self.cursor_position_changed)
+        # self.line_edit.textEdited.connect(self.text_edited)
+        # self.line_edit.editingFinished.connect(self.editing_finished)
+        self.line_edit.selectionChanged.connect(self.selection_changed)
 
         button = QPushButton('Grab data')
         button.clicked.connect(self.button_clicked)
@@ -32,3 +36,15 @@ class Widget(QWidget):
 
     def text_changed(self):
         self.text_holder_label.setText(self.line_edit.text())
+
+    def cursor_position_changed(self, old, new):
+        print(f'old position: {old}  new position: {new}')
+
+    def editing_finished(self):
+        self.text_holder_label.setText(self.line_edit.text())
+
+    def selection_changed(self):
+        self.text_holder_label.setText(self.line_edit.selectedText())
+        
+    def text_edited(self, new_text):
+        print(f'new text: {new_text}')
