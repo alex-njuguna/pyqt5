42640281908d8ed739147339494df994edf436de
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-04 15:43:18 +0300

    define widgets

--- Qlabel-QLineEdit/widget.py
+++ Qlabel-QLineEdit/widget.py
@@ -3,3 +3,11 @@ from PySide6.QtWidgets import QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayo
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle('QLabel and QLineEdit')
+
+        label = QLabel('Fullname: ')
+        self.line_edit = QLineEdit()
+
+        button = QPushButton('Grab data')
+        self.text_holder_label = QLabel('I AM HERE')
