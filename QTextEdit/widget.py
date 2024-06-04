48134fb75e180c1e13809fec509e17cb8e39ce18
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-05 00:21:00 +0300

    define widgets

--- QTextEdit/widget.py
+++ QTextEdit/widget.py
@@ -5,3 +5,30 @@ class Widget(QWidget):
         super().__init__()
 
         self.setWindowTitle('Text Editor')
+
+        self.text_edit = QTextEdit()
+
+        copy_button = QPushButton('copy')
+        copy_button.clicked.connect(self.text_edit.copy)
+
+        cut_button = QPushButton('cut')
+        cut_button.clicked.connect(self.text_edit.cut)
+
+        paste_button = QPushButton('paste')
+        paste_button.clicked.connect(self.text_edit.paste)
+
+        undo_button = QPushButton('undo')
+        undo_button.clicked.connect(self.text_edit.undo)
+
+        redo_button = QPushButton('redo')
+        redo_button.clicked.connect(self.text_edit.redo)
+
+        set_plain_text_button = QPushButton('plain text')
+        set_plain_text_button.clicked.connect(self.set_plain_text)
+
+        set_html_button = QPushButton('html')
+        set_html_button.clicked.connect(self.set_html)
+
+        clear_button = QPushButton('clear')
+        clear_button.clicked.connect(self.text_edit.clear)
+
