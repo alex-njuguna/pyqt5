fc6629730e4436430faa0cc306ffd6bdb291b31f
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-06 00:23:06 +0300

    layout and widgets fro the first section

--- SizePolicies/widget.py
+++ SizePolicies/widget.py
@@ -1,7 +1,14 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QLineEdit, QLabel
+from PySide6.QtWidgets import QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('size policies')
+
+        label = QLabel('some data')
+        line_edit = QLineEdit()
+
+        h_layout_1 = QHBoxLayout()
+        h_layout_1.addWidget(label)
+        h_layout_1.addWidget(line_edit)
