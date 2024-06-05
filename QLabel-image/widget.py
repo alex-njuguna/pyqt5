eb6aee92b6d8fb5739277b6c4a6a59b0785abaab
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-06 00:09:38 +0300

    more adjustments

--- QLabel-image/widget.py
+++ QLabel-image/widget.py
@@ -1,18 +1,24 @@
 import os
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton
-from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QMainWindow
+from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtCore import QSize
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('Label as image')
+        try:
+            self.setWindowIcon(QIcon('images/icon.png'))
+        except Exception as e:
+            print("Error setting window icon:", e)
+        
         self.resize(400, 300)
 
         label = QLabel()
-        image_path = 'images/minions.webp'
+        image_path = 'images/minions.jpg'
         pixmap = QPixmap(image_path)
-        pixmap = pixmap.scaled(400, 300)
+        pixmap = pixmap.scaled(QSize(400, 300))
         label.setPixmap(pixmap)
 
         layout = QVBoxLayout()
