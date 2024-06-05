41f7852408264ab9dab03e372bc6608fa93cc642
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-05 22:53:24 +0300

    define widgets

--- QLabel-image/widget.py
+++ QLabel-image/widget.py
@@ -1,7 +1,13 @@
-from PySide6.QtWidgets import QWidget, QLabel
+import os
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton
+from PySide6.QtGui import QPixmap
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('Label as image')
+
+        label = QLabel()
+        image_path = os.path.join('QLabel-image', 'images/minions.jpg')
+        label.setPixmap(QPixmap(image_path))
