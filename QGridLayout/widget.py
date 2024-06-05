5ff3cb7bd3e221aa072f972415e9e71e6c8321f1
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-06 01:22:59 +0300

    setup grid layout

--- QGridLayout/widget.py
+++ QGridLayout/widget.py
@@ -6,12 +6,28 @@ class Widget(QWidget):
 
         self.setWindowTitle('grid layout')
 
+        # define widgets
         button1 = QPushButton('one')
         button2 = QPushButton('two')
         button3 = QPushButton('three')
+        button3.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         button4 = QPushButton('four')
         button5 = QPushButton('five')
         button6 = QPushButton('six')
         button7 = QPushButton('seven')
 
-        
+        # define layout
+        layout = QGridLayout()
+        layout.addWidget(button1, 0, 0)
+        layout.addWidget(button2, 0, 1, 1, 2)
+        layout.addWidget(button3, 1, 0, 2, 1)
+        layout.addWidget(button4, 1, 1)
+        layout.addWidget(button5, 1, 2)
+        layout.addWidget(button6, 2, 1)
+        layout.addWidget(button7, 2, 2)
+
+        self.setLayout(layout)
+
+
+
+
