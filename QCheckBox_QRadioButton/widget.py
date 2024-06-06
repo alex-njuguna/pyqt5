b6977a6d19833b2aaeb51eeb7e2202e41fcb9858
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-06 22:48:33 +0300

    set layout and widgets for exclusive check boxes

--- QCheckBox_QRadioButton/widget.py
+++ QCheckBox_QRadioButton/widget.py
@@ -26,18 +26,49 @@ class Widget(QWidget):
         os.setLayout(os_layout)
 
 
+        # exclusive checkboxes
+        drinks = QGroupBox('Choose your drink')
+        beer = QCheckBox('beer')
+        tea = QCheckBox('tea')
+        juice = QCheckBox('juice')
+        beer.setChecked(True)
+
+        exclusive_button_group = QButtonGroup(self)
+        exclusive_button_group.addButton(beer)
+        exclusive_button_group.addButton(tea)
+        exclusive_button_group.addButton(juice)
+
+        drinks_layout = QVBoxLayout()
+        drinks_layout.addWidget(beer)
+        drinks_layout.addWidget(tea)
+        drinks_layout.addWidget(juice)
+        drinks.setLayout(drinks_layout)
+
+
+        h_layout = QHBoxLayout()
+        h_layout.addWidget(os)
+        h_layout.addWidget(drinks)
 
         layout = QVBoxLayout()
-        layout.addWidget(os)
+        layout.addLayout(h_layout)
         self.setLayout(layout)
 
 
-    def windows_toggled(self):
-        pass
+    def windows_toggled(self, checked):
+        if checked:
+            print('Windows box checked')
+        else:
+            print('Windows box unchecked')
 
-    def linux_toggled(self):
-        pass
+    def linux_toggled(self, checked):
+        if checked:
+            print('Linux box checked')
+        else:
+            print('Linux box unchecked')
 
-    def mac_toggled(self):
-        pass
+    def mac_toggled(self, checked):
+        if checked:
+            print('Mac box checked')
+        else:
+            print('Mac box unchecked')
 
