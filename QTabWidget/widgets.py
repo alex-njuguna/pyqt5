ebfefb67c80fbf945728b55ab3844d71aea7bead
alex-njuguna <njugunakinuthia013@gmail.com>
2024-06-13 00:30:52 +0300

    sets up information tab

--- QTabWidget/widgets.py
+++ QTabWidget/widgets.py
@@ -1,8 +1,28 @@
 from PySide6.QtWidgets import *
 
-class Widget(QMainWindow):
+class Widget(QWidget):
     "Canvas for the entire app"
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('QTabWidget')
+
+        tab_widget = QTabWidget(self)
+
+        "information form"
+        widget_form = QWidget()
+        label = QLabel('fullname: ')
+        line_edit = QLineEdit()
+
+        widget_form_layout = QHBoxLayout()
+        widget_form_layout.addWidget(label)
+        widget_form_layout.addWidget(line_edit)
+        widget_form.setLayout(widget_form_layout)
+
+
+        tab_widget.addTab(widget_form, 'Information')
+
+
+        layout = QVBoxLayout()
+        layout.addWidget(tab_widget)
+        self.setLayout(layout)
